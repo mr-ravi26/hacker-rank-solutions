3d3eef4649618c8a584e3adaa92790a398ef648a
Ravi B Tripathi <mr.ravi26@gmail.com>
2020-11-06 14:32:35 +0530

    Repeated String

---
+++ javascript/repeated-string.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the repeatedString function below.
+function repeatedString(s, n) {
+
+ const as = s.split("").filter(c => c === "a").length;
+        const times = parseInt(n / s.length);
+        const rest = n % s.length;
+
+        const totalAs = times * as
+            + s.slice(0, rest).split("").filter(c => c === "a").length
+
+        return totalAs; 
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine(), 10);
+
+    let result = repeatedString(s, n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
